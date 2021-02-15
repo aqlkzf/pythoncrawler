a3a0c71db50aa1b1b87328f039027ff739abfdf6
qianlikuaizaifeng <2408982813@qq.com>
2021-02-15 13:10:00 +0800

    33

--- test/testsqlite.py
+++ test/testsqlite.py
@@ -1,5 +1,10 @@
-#-*- coding = utf-8 -*-
-#@Time : 2021-02-14 18:03
-#@Author : QianLiKuaiZaiFeng
-#@File : testsqlite.py
-#@Software : PyCharm
+# -*- coding = utf-8 -*-
+# @Time : 2021-02-14 18:03
+# @Author : QianLiKuaiZaiFeng
+# @File : testsqlite.py
+# @Software : PyCharm
+
+
+import sqlite3
+conn=sqlite3.connect("test.db")          #打开或创建数据库文件
+print("Opened database successfully")
